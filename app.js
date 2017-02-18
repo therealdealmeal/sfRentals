33280dd88edaf3da14ac71e35478323cd19be5a4
alejandro <realdeal@Vishals-MacBook-Pro.local>
2017-02-17 17:33:12 -0800

    very small changes if none

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('sfRental',['ngRoute']).
+var myApp = angular.module('myApp',['ngRoute'])
 
 myApp.config(['$routeProvider', function($routeProvider, $locationProvider){
 
