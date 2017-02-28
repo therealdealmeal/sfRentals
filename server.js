c78b760bb350fbd5ccb8b8e63e4786d6fe6fd6d5
alejandro <realdeal@Vishals-MacBook-Pro.local>
2017-02-27 19:42:02 -0800

    got button to show/hide form; installed nodemailer and is working; problem is that it's not getting any of the data entered in the form

--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, '.')));
+
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'boceltic2000@gmail.com',
+        pass: ''
+    }
+});
+
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+app.post('/send', function (req, res) {
+  var mailOptions = {
+      from: req.query.mail, // sender address
+      to: 'boceltic2000@gmail.com', // list of receivers
+      subject: 'Rental Inquiry', // Subject line
+      text: req.query.message
+  }
+  console.log(mailOptions);
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Email successfully sent!');
+  });
+});
+
 
 // app.use('/', function(req, res) {
 //   res.send("Checking Express program");
