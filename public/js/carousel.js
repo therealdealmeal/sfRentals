1e4e5f3cc3e0e45cf7565cc6eb34f448cac9f1b6
alejandro <realdeal@Vishals-MacBook-Pro.local>
2017-05-15 21:34:05 -0700

    adujusted carousel position; editted location; changed home font

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#myCarousel").carousel({interval: 3000});
+  $("#myCarousel").carousel({interval: 1500});
 
   $(".item1").click(function(){
     $("#myCarousel").carousel(0);
